7c3d91dfaf781e3acaaf734924e2a8277cbb7fc6
UraniumFruit <75060552+ThePala@users.noreply.github.com>
2022-07-24 11:55:55 +0530

    create cbse.py

---
+++ cbse.py
@@ -0,0 +1,10 @@
+t1 = int(input("How much you got in term 1 : "))
+print()
+print()
+tot = int(input("What is your final THEORY marks in that subject:? "))
+print()
+
+z = (tot - 0.6 * t1) / 1.4
+print()
+print()
+print("you got", z, "marks in term2")
